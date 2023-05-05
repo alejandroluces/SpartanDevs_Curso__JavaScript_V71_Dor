f2628b6472cc33f62a7e4627f7c0e1a32feee47b
Alejandro Luces González <aluces@polytex.local>
2023-05-05 11:54:32 -0400

    Adding V_14.3 JavaScript Course_Exercise

---
+++ V_14.3_exercise.js
@@ -0,0 +1,13 @@
+
+
+// Solicitar un número X al usuario
+const numero = prompt("Ingrese un número:");
+
+// Iterar desde 1 hasta el número ingresado por el usuario
+for (let i = 1; i <= numero; i++) {
+
+  const parOImpar = i % 2 === 0 ? "par" : "impar";//valida si es par o impar (operador ternario)
+  console.log(`${i}   es ${parOImpar}`); // Imprimir el resultado
+  
+}
+
