bfefec9e0f521b5920fddabfee6bbaa58889e2ac
Alejandro Luces González <aluces@polytex.local>
2023-05-05 09:48:27 -0400

    Adding V_14.2 JavaScript Course_Exercise

--- V_14.2_exercise.js
+++ V_14.2_exercise.js
@@ -1,42 +1,45 @@
+const figura= prompt("¿Qué figura desea calcular el área? (triangulo, rectangulo, circulo)");
 
-// Funciones para calcular el área de cada figura
-function areaTriangulo(base, altura) {
-    return (base * altura) / 2;
-  }
-  
-  function areaRectangulo(base, altura) {
-    return base * altura;
-  }
-  
-  function areaCirculo(radio) {
-    return Math.PI * Math.pow(radio, 2);
-  }
-  
-  // Preguntar al usuario qué figura desea calcular
-  const figura = prompt("¿Qué figura desea calcular el área? (triángulo, rectángulo, círculo)");
-  
-  switch (figura.toLowerCase()) {
-    case "triángulo":
-      const baseTriangulo = parseFloat(prompt("Ingrese la base del triángulo"));
-      const alturaTriangulo = parseFloat(prompt("Ingrese la altura del triángulo"));
-      const areaT = areaTriangulo(baseTriangulo, alturaTriangulo);
-      console.log(`El área del triángulo es: ${areaT}`);
-      break;
-  
-    case "rectángulo":
-      const baseRectangulo = parseFloat(prompt("Ingrese la base del rectángulo"));
-      const alturaRectangulo = parseFloat(prompt("Ingrese la altura del rectángulo"));
-      const areaR = areaRectangulo(baseRectangulo, alturaRectangulo);
-      console.log(`El área del rectángulo es: ${areaR}`);
-      break;
-  
-    case "círculo":
-      const radioCirculo = parseFloat(prompt("Ingrese el radio del círculo"));
-      const areaC = areaCirculo(radioCirculo);
-      console.log(`El área del círculo es: ${areaC}`);
-      break;
-  
+
+switch(figura){
+case "triangulo":
+    const baseTriangulo=prompt("Ingrese la base del triangulo");
+    const alturatriangulo=prompt("Ingrese la altura del triangulo");
+    let areaTriangulo= (baseTriangulo*alturatriangulo) / 2;
+    const areaT= areaTriangulo;
+    console.log(`El area del triangulo es: ${areaT}`)
+    break
+
+case "rectangulo":
+    const baseRectangulo=prompt("Ingrese la base del rectangulo");
+    const alturaRectangulo=prompt("Ingrese la altura del rectangulo");
+    let areaRectangulo= (baseRectangulo*alturaRectangulo);
+    const areaR= areaRectangulo;
+    console.log(`El area del rectangulo es: ${areaR}`)
+    break
+case "circulo":
+    const radioCirculo=prompt("Ingrese el radio del circulo");
+    let areaCirculo = (Math.PI * Math.pow(radioCirculo,2));
+    areaC= areaCirculo;
+    console.log(`El area del circulo es: ${areaC}`);
+    break
+// Esto es para cuando la palabra de la figura no coincida
     default:
-      console.log("Figura no válida");
-  }
-  
+    console.log("Figura no válida");
+    
+}
+
+
+/*
+const : Nos permite declarar variables inmutables, 
+osea no se puede cambiar. var y 
+let : Nos permiten crear variables mutables que si se puede cambiar.
+*/
+
+
+// let areaRectangulo= (base*altura);
+
+
+
+
+
