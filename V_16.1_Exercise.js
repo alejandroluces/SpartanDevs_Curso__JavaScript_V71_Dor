f4bb3bfb2feb83489cdee77a354a0989796f33ab
Alejandro Luces González <aluces@polytex.local>
2023-05-08 17:15:24 -0400

    Adding Exercise V_16.1  JavaScript Course_Exercise

---
+++ V_16.1_Exercise.js
@@ -0,0 +1,9 @@
+let suma=0;
+let contador=0;
+
+while(suma<50){
+    suma+= parseInt(prompt("Introduce un numero para añadir a la suma"));
+    contador++
+}
+console.log(`La suma total de los tres numeros es:  ${suma}`);
+console.log (`El total de numeros introducidos en :  ${contador}`);
