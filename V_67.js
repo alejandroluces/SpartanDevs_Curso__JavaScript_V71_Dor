e7561fb33a89e1d68300e1b2767a3f4cb773d27b
Alejandro Luces González <aluces@polytex.local>
2023-06-03 17:06:59 -0400

    Adding Exercise V_67 JavaScript Course_Exercise

---
+++ V_67.js
@@ -0,0 +1,53 @@
+/*
+
+https : //developer .mozilla .org/es/docs/Web/HavaScript/Referencia/ Operadores/Destructuring_assignment
+La destructuración es una expresión de JavaScript que hace posible la extracción de datos de arreglos u objetos 
+
+*/
+
+const person={
+    name:"Alejo",
+    age:20,
+    email: "aluces@gmail.com"
+
+}
+/* 
+const name = person.name
+const age = person.age
+const email = person.email
+*/
+
+// Destructuring
+// Otro ejemplo
+/* 
+const{name,age,email}=person
+console.log(name,age,email);
+*/
+
+/* 
+const numbers=[1,2,3,4,5]
+
+const[a,segundaPosicion] =numbers
+
+console.log(segundaPosicion);
+*/
+// Ejemplo
+/* 
+const printPerson=({name})=>{
+    console.log(name);
+}
+printPerson(person)
+
+*/
+/* 
+const printPerson=({name:nombre})=>{
+    console.log(nombre);
+}
+printPerson(person)
+*/
+
+const getUsers = async ()=>{
+    const {data:users}= await axios.get("https://jsonplaceholder.typicode.com/users")
+    console.log(users.data)
+}
+getUsers()
